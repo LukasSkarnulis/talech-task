5225fafa821ead38e09cbdeb04abf63ad451f92a
LuksiusLukas2 <luksiuslukas2@gmail.com>
2020-03-30 20:27:33 +0300

    Updates

---
+++ precache-manifest.7ee267b5b4229be114d8601fb4b56a27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca64fa844fb14e1eab0bee27ef6305fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74209530dd8f1220add8",
+    "url": "/static/css/main.1a5c35b2.chunk.css"
+  },
+  {
+    "revision": "df5df4fe83c23985b0ae",
+    "url": "/static/js/2.027daeb0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.027daeb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74209530dd8f1220add8",
+    "url": "/static/js/main.c119c523.chunk.js"
+  },
+  {
+    "revision": "433b9e78b52e954f7680",
+    "url": "/static/js/runtime-main.f2128e9d.js"
+  }
+]);
